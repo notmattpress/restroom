05b65c7a2426b6dd9574b4eddf460e2b02d627c8
Not Paul <notpaul@mattsmomshouse.com>
2025-03-15 04:35:32 +0000

    Automated sync and customization of Restroom

--- e2e/specs/browser.test.js
+++ e2e/specs/browser.test.js
@@ -1,10 +1,10 @@
-describe( 'Storefront', () => {
+describe( 'Restroom', () => {
 	beforeAll( async () => {
 		await page.goto( STORE_URL );
 	} );
 
-	it( 'should have "built with WooCommerce" footer', async () => {
+	it( 'should have "built with PooCommerce" footer', async () => {
 		const footerText = await page.evaluate( () => document.querySelector( 'body' ).innerText );
-		expect( footerText ).toMatch( 'Built with WooCommerce.' );
+		expect( footerText ).toMatch( 'Built with PooCommerce.' );
 	} );
 } );
