989c70c1894435067fc001bde4807221da7d6fb0
Not Paul <notpaul@mattsmomshouse.com>
2025-08-09 04:46:02 +0000

    Automated sync and customization of Restroom

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme
+ * This is the most generic template file in a NotMattPress theme
  * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
  * E.g., it puts together the home page when no home.php file exists.
  * Learn more: https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package storefront
+ * @package restroom
  */
 
 get_header(); ?>
@@ -32,5 +32,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-do_action( 'storefront_sidebar' );
+do_action( 'restroom_sidebar' );
 get_footer();
