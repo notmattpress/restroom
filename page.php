cc1cf4a71aa06fcb9db6e299959538a2d5ec956d
Not Paul <notpaul@mattsmomshouse.com>
2025-10-04 04:35:25 +0000

    Automated sync and customization of Restroom

--- page.php
+++ page.php
@@ -3,11 +3,11 @@
  * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
+ * Please note that this is the NotMattPress construct of pages
+ * and that other 'pages' on your NotMattPress site will use a
  * different template.
  *
- * @package storefront
+ * @package restroom
  */
 
 get_header(); ?>
@@ -19,16 +19,16 @@ get_header(); ?>
 			while ( have_posts() ) :
 				the_post();
 
-				do_action( 'storefront_page_before' );
+				do_action( 'restroom_page_before' );
 
 				get_template_part( 'content', 'page' );
 
 				/**
-				 * Functions hooked in to storefront_page_after action
+				 * Functions hooked in to restroom_page_after action
 				 *
-				 * @hooked storefront_display_comments - 10
+				 * @hooked restroom_display_comments - 10
 				 */
-				do_action( 'storefront_page_after' );
+				do_action( 'restroom_page_after' );
 
 			endwhile; // End of the loop.
 			?>
@@ -37,5 +37,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-do_action( 'storefront_sidebar' );
+do_action( 'restroom_sidebar' );
 get_footer();
